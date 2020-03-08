2d1666c6358f7e483a92121867d2f10e1662410b
Shanb007 <60854496+Shanb007@users.noreply.github.com>
2020-03-08 23:53:27 +0530

    Update tsp.cpp

--- tsp.cpp
+++ tsp.cpp
@@ -252,46 +252,30 @@ string startSearch()
 	return res;
 }
 
-
-
 int main()
-{
-	TSP prob1;
-	int i;
-	prob1.inputData();
-	prob1.createOriginalDistanceGraph();// Till now we have the orginal Distance Graph
-	int numCities=prob1.numCities;
+{ 
+ start=1;
+lli visited[V1+1]={0};
+ vector<lli>path; 
+ lli graph[][V1] = 
+ { 
+{0, 29, 82, 46, 68, 52, 72, 42, 51, 55, 29, 74, 23, 72, 46,},
+{29, 0, 55, 46, 42, 43, 43, 23, 23, 31, 41, 51, 11, 52, 21,}, 
+{82, 55, 0, 68, 46, 55, 23, 43, 41, 29, 79, 21, 64, 31, 51,}, 
+{46, 46, 68, 0, 82, 15, 72, 31, 62, 42, 21, 51, 51, 43, 64,}, 
+{68, 42, 46, 82, 0, 74, 23, 52, 21, 46, 82, 58, 46, 65, 23,}, 
+{52, 43, 55, 15, 74, 0, 61, 23, 55, 31, 33, 37, 51, 29, 59,}, 
+{72, 43, 23, 72, 23, 61, 0, 42, 23, 31, 77, 37, 51, 46, 33,}, 
+{42, 23, 43, 31, 52, 23, 42, 0, 33, 15, 37, 33, 33, 31, 37,}, 
+{51, 23, 41, 62, 21, 55, 23, 33, 0, 29, 62, 46, 29, 51, 11,}, 
+{55, 31, 29, 42, 46, 31, 31, 15, 29, 0, 51, 21, 41, 23, 37,}, 
+{29, 41, 79, 21, 82, 33, 77, 37, 62, 51, 0, 65, 42, 59, 61,}, 
+{74, 51, 21, 51, 58, 37, 37, 33, 46, 21, 65, 0, 61, 11, 55,}, 
+{23, 11, 64, 51, 46, 51, 51, 33, 29, 41, 42, 61, 0, 62, 23,}, 
+{72, 52, 31, 43, 65, 29, 46, 31, 51, 23, 59, 11, 62, 0, 59,}, 
+{46, 21, 51, 64, 23, 59, 33, 37, 11, 37, 61, 55, 23, 59, 0,}
+ }; 
+ traversal(start,0,visited,path,graph);
+ return 0; 
+ }
 
-	vector<int> v1;
-	vector<char> v2;
-
-	for( i=1;i<numCities;i++)
-	{
-		v1.push_back(i);
-		if(i>25)
-		{
-			v2.push_back('a'+i-26);
-		}
-		else
-		{
-			v2.push_back('A'+i);
-		}
-	}
-	vector<char>:: iterator it=v2.begin();
-	int initialHeuristic=calculateHeuristic(v1,v2,0);
-
-
-
-	City[nodeTrack++]=createNode(0,"A",numCities-1,'A',initialHeuristic,0,initialHeuristic,"AA",v1);
-
-
-
-	astar.push(City[0]);
-	string res=startSearch();
-
-	int lengthMst = buildMinimumSpanningTree2(v1,v2);
-
-	cout<<"\n the optimumCost is "<<optimumCost<<" with path "<<res+'A'<<"\n";
-	cout<<"the number of nodes expanded are "<<numExpanded<<" total nodes are "<<totalNumofNodes<<"\n";
-	return 0;  
-}
